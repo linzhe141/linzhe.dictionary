e2e3f837e6c293ea8514538e29185aedbffe87db
linzhe141 <1572213544@qq.com>
2024-07-26 13:12:00 +0800

    chore: vocabulary cheat sheet  orderBy desc id

--- server/api/vocabularyCheatSheet/index.get.ts
+++ server/api/vocabularyCheatSheet/index.get.ts
@@ -1,7 +1,10 @@
+import { desc } from 'drizzle-orm'
+
 export default eventHandler(async () => {
   const todos = await useDrizzle()
     .select()
     .from(tables.vocabularyCheatSheet)
+    .orderBy(desc(tables.vocabularyCheatSheet.id))
     .all()
 
   return todos
