441a1b5615008e4bce68b892f8ecf364c1188dd5
linzhe141 <1572213544@qq.com>
2025-10-29 10:45:34 +0800

    feat: enhance auth middleware

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,7 +1,12 @@
-export default defineNuxtRouteMiddleware(() => {
+export default defineNuxtRouteMiddleware((to, from) => {
+  console.group('auth middleware')
+  console.info('to->', to)
+  console.info('from->', from)
+  console.groupEnd()
+
   const user = useCookie('user')
   if (!user.value) {
-    const uri = location.pathname.slice(1) + location.search
+    const uri = to.fullPath
     return navigateTo('/login?redirect=' + encodeURIComponent(uri))
   }
 })
